d6fc71b77e8219a884456bdf1f0c0a66e993dcb2
lucasseblanco <69616137+lucasseblanco@users.noreply.github.com>
2022-09-13 19:30:59 -0300

    commit 1

---
+++ clase 1/index.js
@@ -0,0 +1,23 @@
+const nombre = 'Ramiro';
+
+const peliculas = {
+    'anio': '2012',
+    'nombre': 'Pelicula',
+    'personajes': ['uno', 'dos', 'tres']
+};
+
+let edad = 25;
+
+const personajesParaPush = ['cuatro', 'cinco', 'seis'];
+
+personajesParaPush.push('siete')
+
+console.log({nombre});
+console.log({peliculas});
+console.log({edad});
+console.log({personajesParaPush});
+
+
+
+
+
